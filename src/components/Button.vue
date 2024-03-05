16270316615a56ef18798dbeb769d963faac40a3
csandrisTurr <csaszarandras@turr.hu>
2024-03-05 14:13:44 +0100

    BOT-8: gonb

---
+++ src/components/Button.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps(['text']);
+</script>
+
+<template>
+    <span class="bg-teal-500 hover:bg-teal-400 duration-200 cursor-pointer select-none active:bg-teal-600 px-3 py-2 rounded-sm shadow text-neutral-50 font-medium">
+        {{ text }}
+    </span>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import Button from '../components/Button.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+      <div class="flex flex-col w-60 gap-2">
+        <Button text="afdgfdgfdg"></Button>
+        <Button text="asdsdadsa"></Button>
+        <Button text="grgergergg"></Button>
+      </div>
   </main>
 </template>
