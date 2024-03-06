659b7d112ab2153a690370226d2055056b4b4414
csandrisTurr <csaszarandras@turr.hu>
2024-03-06 09:24:29 +0100

    BOT-10: added icon

--- src/components/NumericUpDown.vue
+++ src/components/NumericUpDown.vue
@@ -16,20 +16,20 @@ function down() {
 </script>
 
 <template>
-    <span class="flex border-teal-500 border-2 hover:border-teal-400 duration-200 active:border-teal-600 rounded-sm shadow">
+    <span class="flex border-teal-500 border-2 hover:border-teal-400 duration-200 rounded-sm shadow">
         <input
             id="input"
             class="cursor-text select-none px-3 py-2 text-neutral-800 font-medium outline-none border-none"
             type="text" name="example" v-model="contentModel" />
         <span class="flex flex-col cursor-pointer select-none">
-            <span class="op" @click="up">/\</span>
-            <span class="op" @click="down">\/</span>
+            <span class="op material-symbols-outlined" @click="up">keyboard_arrow_up</span>
+            <span class="op material-symbols-outlined" @click="down">keyboard_arrow_down</span>
         </span>
     </span>
 </template>
 
 <style lang="scss">
 .op {
-    @apply px-3 text-sm duration-200 font-bold text-teal-600;
+    @apply px-3 text-sm duration-200 font-bold text-teal-600 hover:text-teal-400 active:text-teal-700;
 }
 </style>
