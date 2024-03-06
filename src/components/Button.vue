97d7aca4e3164f604754bbb8b9753711d167df45
csandrisTurr <csaszarandras@turr.hu>
2024-03-06 08:18:53 +0100

    button styling reorder

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,7 +3,7 @@ defineProps(['text']);
 </script>
 
 <template>
-    <span class="bg-teal-500 hover:bg-teal-400 duration-200 cursor-pointer select-none active:bg-teal-600 px-3 py-2 rounded-sm shadow text-neutral-50 font-medium">
+    <span class="bg-teal-500 hover:bg-teal-400 active:bg-teal-600 text-neutral-50 duration-200 cursor-pointer select-none px-3 py-2 rounded-sm shadow font-medium">
         {{ text }}
     </span>
 </template>
