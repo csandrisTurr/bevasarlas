8fd0e0de7b1049a7594236959ef2de6a86bb84ce
csandrisTurr <csaszarandras@turr.hu>
2024-03-06 08:39:01 +0100

    added model to input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 defineProps(['text']);
+const contentModel = defineModel('content');
 </script>
 
 <template>
     <input 
         class="border-teal-500 border-2 hover:border-teal-400 duration-200 active:border-teal-600 cursor-text select-none px-3 py-2 rounded-sm shadow text-neutral-800 font-medium outline-teal-500"
-        type="text" name="" id="">
+        type="text"
+        name=""
+        id=""
+        v-model="contentModel"
+        \>
 </template>
