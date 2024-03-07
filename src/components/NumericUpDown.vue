2cbaae6d37554347caf2b2a6edbb4873d650015e
csandrisTurr <csaszarandras@turr.hu>
2024-03-07 13:19:44 +0100

    negativ numericupdown handle

--- src/components/NumericUpDown.vue
+++ src/components/NumericUpDown.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-const props = defineProps(['text']);
+const props = defineProps({
+  text: String,
+  can_go_negative: Boolean,  
+});
 const contentModel = defineModel(['content'])
 
 function up() {
@@ -11,12 +14,18 @@ function up() {
 function down() {
     const input = document.getElementById('input');
     input.value = Number(input.value) - 1;
+
+    // handle negative
+    if (!props.can_go_negative && input.value < 0) {
+        input.value = 0;
+    }
+
     input.dispatchEvent(new Event('input'));
 }
 </script>
 
 <template>
-    <span class="flex border-teal-500 border-2 hover:border-teal-400 duration-200 rounded-sm shadow w-full">
+    <span class="flex border-teal-500 border-2 hover:border-teal-400 duration-200 rounded-sm focus:rounded-sm shadow w-full">
         <input
             id="input"
             class="cursor-text select-none px-3 py-2 text-neutral-800 font-medium outline-none border-none w-full"
