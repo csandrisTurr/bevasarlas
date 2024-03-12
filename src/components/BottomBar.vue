ebbbe641d63d6b83f9b98895e1e81c42ae6c9b1c
csandrisTurr <csaszarandras@turr.hu>
2024-03-12 13:35:52 +0100

    BOT-22: mua reszponzivitas

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -6,47 +6,57 @@ import Button from '@/components/Button.vue';
 
 <template>
     <main class="p-4 bg-neutral-100 border-t-[1px]">
-        <div class="flex gap-4 justify-stretch flex-wrap">
-        <span class="field">
-          <span>Kategoria</span>
-          <ComboBox></ComboBox>
-        </span>
-        <span class="field">
-          <span>Termek</span>
-          <ComboBox></ComboBox>
-        </span>
-        <span class="field">
-          <span>Mennyiseg</span>
-          <NumericUpDown></NumericUpDown>
-        </span>
-        <span class="field">
-          <span>Reszosszeg</span>
-          <span class="label">
-            <span>1 000 Ft</span>
+      <div class="flex gap-4 justify-stretch max-md:flex-wrap">
+        <div class="fields flex gap-4 justify-stretch w-3/6 max-md:w-full">
+          <span>
+            <span>Kategoria</span>
+            <ComboBox></ComboBox>
           </span>
-        </span>
-        <span class="field">
-          <span>Vegosszeg</span>
-          <span class="label">
-            <span>1 000 Ft</span>
+          <span>
+            <span>Termek</span>
+            <ComboBox></ComboBox>
           </span>
-        </span>
-        <span class="field justify-end sm:w-full">
-          <span>⠀</span>
-          <Button class="material-symbols-outlined text-center h-max flex-grow sm">add</Button>
-        </span>
+          <span>
+            <span>Mennyiseg</span>
+            <NumericUpDown></NumericUpDown>
+          </span>
+        </div>
+        <div class="labels flex gap-4 justify-evenly w-2/6 max-md:w-full max-md:justify-around">
+          <span class="field">
+            <span>Reszosszeg</span>
+            <span class="label">
+              <span>1 000 Ft</span>
+            </span>
+          </span>
+          <span class="field">
+            <span>Vegosszeg</span>
+            <span class="label">
+              <span>1 000 Ft</span>
+            </span>
+          </span>
+        </div>
+        <div class="flex flex-col gap-1 max-md:w-full lg-max-w-[200px] w-1/6">
+          <span class="flex flex-col justify-end w-full">
+            <span class="max-md:hidden">⠀</span>
+            <Button class="material-symbols-outlined text-center h-max flex-grow">add</Button>
+          </span>
+        </div>
       </div>
     </main>
 </template>
 
 <style scoped lang="scss">
 
-.field {
-  @apply flex flex-col gap-1 md:w-1/6;
+.fields > span, .labels > span {
+  @apply flex flex-col gap-1;
 
   & > span {
     @apply font-medium;
   }
+}
+
+.fields > span {
+  @apply w-1/3;
 }
 
 .label {
